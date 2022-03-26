41a8e2c1cb3e3820aaf9c1a515f29b4568bad9b7
MrCommit <maxirybakin@gmail.com>
2022-03-26 14:26:47 -0700

    Models and Seeds changes

--- models/User.js
+++ models/User.js
@@ -1,53 +1,54 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bycrpt');
-const sequelize = require ('../config/connection');
-const { Z_DEFAULT_STRATEGY } = require('zlib');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
 class User extends Model {
-    checkPassword(loginPw) {
-        return bcrypt.compareSync(loginPw, this.password);
-    }
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
 }
 
 User.init(
-    {
-        userID: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            }
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [8, 20],
-            }
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        hooks: {
-            beforeCreate: async (newUser) => {
-                newUser.password = await bcrypt.hash(user.password, salt);
-                return newUser;
-            }
-        },
-        sequelize,
-        modelName: 'user'
-    }
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
 );
 
 module.exports = User;
---
+++ seeds/paintingData.js
@@ -0,0 +1,144 @@
+const { Painting } = require('../models');
+
+const paintingdata = [
+  {
+    title: 'HTML Introduction',
+    artist: 'LedyX',
+    exhibition_date: 'March 30, 2018',
+    gallery_id: 1,
+    filename: '01-blossoming-apricot.jpg',
+    description:
+      'Branches with pink apricot blossoms against a blue background.',
+  },
+  {
+    title: 'HTML Elements',
+    artist: 'WStudio',
+    exhibition_date: 'May 05, 2017',
+    gallery_id: 1,
+    filename: '02-cosmos-flowers.jpg',
+    description: 'Pink cosmos flowers against a blue sky.',
+  },
+  {
+    title: 'HTML Attributes',
+    artist: 'WStudio',
+    exhibition_date: 'May 05, 2017',
+    gallery_id: 1,
+    filename: '02-cosmos-flowers.jpg',
+    description: 'Pink cosmos flowers against a blue sky.',
+  },
+  {
+    title: 'HTML Comments',
+    artist: 'WStudio',
+    exhibition_date: 'May 05, 2017',
+    gallery_id: 1,
+    filename: '02-cosmos-flowers.jpg',
+    description: 'Pink cosmos flowers against a blue sky.',
+  },
+  {
+    title: 'CSS Introduction',
+    artist: 'S_Photo',
+    exhibition_date: 'June 10, 2019',
+    gallery_id: 2,
+    filename: '03-sand-sea-summer.jpg',
+    description: 'Sandy beach with the blue sea and sky in the background.',
+  },
+  {
+    title: 'CSS Syntax',
+    artist: 'icemanphotos',
+    exhibition_date: 'July 4, 2020',
+    gallery_id: 2,
+    filename: '04-beach-chairs.jpg',
+    description: 'Two beach chairs under a beach umbrella on the beach.',
+  },
+  {
+    title: 'CSS Selectors',
+    artist: 'icemanphotos',
+    exhibition_date: 'July 4, 2020',
+    gallery_id: 2,
+    filename: '04-beach-chairs.jpg',
+    description: 'Two beach chairs under a beach umbrella on the beach.',
+  },
+  {
+    title: 'CSS Box Model',
+    artist: 'icemanphotos',
+    exhibition_date: 'July 4, 2020',
+    gallery_id: 2,
+    filename: '04-beach-chairs.jpg',
+    description: 'Two beach chairs under a beach umbrella on the beach.',
+  },
+  {
+    title: 'JavaScript Introduction',
+    artist: 'VRstudio',
+    exhibition_date: 'August 14, 2016',
+    gallery_id: 3,
+    filename: '05-beach-sunrise.jpg',
+    description: 'Sun setting in the horizon with waves lapping the shore.',
+  },
+  {
+    title: 'JavaScript Statements',
+    artist: 'DrivingJack',
+    exhibition_date: 'October 15, 2018',
+    gallery_id: 3,
+    filename: '06-fall-colors.jpg',
+    description:
+      'Trees with red, orange, yellow leaves reflected on a still lake.',
+  },
+  {
+    title:  'JavaScript Syntax',
+    artist: 'Vitalii_Mamchuk',
+    exhibition_date: 'November 3, 2016',
+    gallery_id: 3,
+    filename: '07-autumn-mountains.jpg',
+    description:
+      'Mountains with red and yellow leaves against a background of hazy rolling hills.',
+  },
+  {
+    title: 'JavaScript Objects',
+    artist: 'Vlad Sokolovsky',
+    exhibition_date: 'December 24, 2020',
+    gallery_id: 3,
+    filename: '08-frozen-river.jpg',
+    description:
+      'Trees with white frozen branches reflected on a frozen river against a light pink sky.',
+  },
+  {
+    title: 'SQL Intro',
+    artist: 'Smit',
+    exhibition_date: 'January 20, 2018',
+    gallery_id: 4,
+    filename: '09-winter-home.jpg',
+    description:
+      'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
+  },
+  {
+    title: 'SQL Database',
+    artist: 'Smit',
+    exhibition_date: 'January 20, 2018',
+    gallery_id: 4,
+    filename: '09-winter-home.jpg',
+    description:
+      'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
+  },
+  {
+    title: 'SQL Syntax',
+    artist: 'Smit',
+    exhibition_date: 'January 20, 2018',
+    gallery_id: 4,
+    filename: '09-winter-home.jpg',
+    description:
+      'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
+  },
+  {
+    title: 'SQL Select',
+    artist: 'Smit',
+    exhibition_date: 'January 20, 2018',
+    gallery_id: 4,
+    filename: '09-winter-home.jpg',
+    description:
+      'Log cabin blanketed in heavy white snow with tall snow covered pine trees in the background.',
+  }
+];
+
+const seedPaintings = () => Painting.bulkCreate(paintingdata);
+
+module.exports = seedPaintings;
