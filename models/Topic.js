dddb7f1e73c99968731eaef1d05b3ff31b314d05
bEbOuNdLeSs <beboundlessssss@gmail.com>
2022-03-28 18:20:45 -0700

    Renaming handlebars

--- models/Topic.js
+++ models/Topic.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Gallery extends Model {}
+class Topic extends Model {}
 
-Gallery.init(
+Topic.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -28,8 +28,8 @@ Gallery.init(
     sequelize,
     freezeTableName: true,
     underscored: true,
-    modelName: 'gallery',
+    modelName: 'topic',
   }
 );
 
-module.exports = Gallery;
+module.exports = Topic;
--- seeds/topicData.js
+++ seeds/topicData.js
@@ -1,6 +1,6 @@
-const { Gallery } = require('../models');
+const { Topic } = require('../models');
 
-const gallerydata = [
+const topicdata = [
   {
     name: 'HTML',
     starting_date: 'April 20, 2021 07:00:00',
@@ -23,6 +23,6 @@ const gallerydata = [
   },
 ];
 
-const seedGallery = () => Gallery.bulkCreate(gallerydata);
+const seedTopic = () => Topic.bulkCreate(topicdata);
 
-module.exports = seedGallery;
+module.exports = seedTopic;
