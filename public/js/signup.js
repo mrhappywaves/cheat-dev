0f861126463190fd89e64eabf34ef754fa9dec9d
MrCommit <maxirybakin@gmail.com>
2022-04-02 02:26:27 -0700

    Changing Login and Signup styling and logic

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,43 +1,25 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    const usernameEl = document.querySelector('#username-signup');
-    const emailEl = document.querySelector('#email-signup');
-    const passwordEl = document.querySelector('#password-signup');
+  event.preventDefault();
 
-    fetch('/api/users', {
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && email && password) {
+    const response = await fetch('/api/users', {
       method: 'POST',
-      body: JSON.stringify({
-        username: usernameEl.value,
-        email: emailEl.value,
-        password: passwordEl.value
-      }),
-      headers: { 'Content-Type': 'application/json'}
-    })
-      .then(function() {
-        document.location.replace('/');
-      })
-      .catch (err => console.log(err));
-  };
-  
-  //   if (username && email && password) {
-  //     const response = await fetch('/api/users', {
-  //       method: 'POST',
-  //       body: JSON.stringify({ username, email, password }),
-  //       headers: { 'Content-Type': 'application/json' },
-  //     });
-  
-  //     if (response.ok) {
-  //       console.log('success');
-  //       document.location.replace('/topic');
-  //     } else {
-  //       alert('Failed to sign up.');
-  //     }
-  //   }
-  // };
-  
+      body: JSON.stringify({ username, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
 
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to sign up.');
+    }
+  }
+};
+  
   document
     .querySelector('.signup-form')
     .addEventListener('submit', signupFormHandler);
-
